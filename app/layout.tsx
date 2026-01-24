5e3478108b436124f0a6fe5418b428d0ba67083e
Ali <ali@MacBookPro.docsis.vodafone.cz>
2026-01-24 22:26:50 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next"
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import LangSetter from "./LangSetter"
@@ -58,6 +59,7 @@ export default function RootLayout({
         <LangSetter />
         <AnimationManager />
         {children}
+        <Analytics />
       </body>
     </html>
   )
