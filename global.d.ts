29ffa116f4ff3d5f592c20697e870f76c54774f9
Adam Wolf <adam.wolf768@student.cuni.cz>
2026-03-06 23:35:43 +0100

    Fix global.d.ts: use script-scoped declarations for both anime module and Window

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,6 @@
 declare module "animejs/lib/anime.es.js";
 
-export {};
-
-declare global {
-    interface Window {
-        gtag: (...args: any[]) => void;
-        dataLayer: Record<string, any>[];
-    }
+interface Window {
+    gtag: (...args: any[]) => void;
+    dataLayer: Record<string, any>[];
 }
